a5a33f99adbcf7f056070d921afa04c129088d88
Sacaldur <sacaldur@gmx.net>
2014-10-25 20:18:27 +0200

    FollowCamera
    Camera script to follow the player

---
+++ unity/Assets/Camera/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FollowCamera : MonoBehaviour {
+
+	private Transform _transform;
+
+	[SerializeField]
+	private Transform followed;
+	private float distance;
+
+	void Awake() {
+		this._transform = this.transform;
+		this.distance = this._transform.position.z;
+	}
+
+	void Update () {
+		if(this.followed != null) {
+			Vector3 position = this.followed.position;
+			position.z = this.distance;
+			this._transform.position = position;
+		}
+	}
+}

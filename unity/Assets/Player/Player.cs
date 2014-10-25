e06cd1219ad8faa15ea9c4f6d46f7440fe6dfe5f
Moritz Schäfer <mail@moritzs.de>
2014-10-25 17:05:28 +0200

    add support for mouse rotation to player

--- unity/Assets/Player/Player.cs
+++ unity/Assets/Player/Player.cs
@@ -18,12 +18,20 @@ public class Player : MonoBehaviour {
 
 
 	void Start () {
-		
+
 	}
 
 	void Update () {
 		float hor = Input.GetAxis("Horizontal");
 		float vert = Input.GetAxis("Vertical");
+    Vector3 mouseWorldPoint =Camera.main.ScreenToWorldPoint((Vector3)(Input.mousePosition));
+    // calculate the view angle
+    Vector3 viewVector  = (mouseWorldPoint - this._transform.position);
+    Vector3 angles = this._transform.eulerAngles;
+    angles.z = (180.0f*Mathf.Atan2(-viewVector.x, viewVector.y)/Mathf.PI);
+    this._transform.eulerAngles = angles;
+
+
 		Vector2 movement = new Vector2(hor, vert) * this.movementSpeed;
 		this._rigidbody.velocity = movement;
 	}

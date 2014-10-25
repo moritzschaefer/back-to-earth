4bac087c958763d7a7f8610c657f1b9e51fb93cd
Sacaldur <sacaldur@gmx.net>
2014-10-25 16:11:04 +0200

    Player Movement
    The Player can Move and collide with other colliders
    Collision layers are not defined yet

---
+++ unity/Assets/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	private Transform _transform;
+	private Rigidbody2D _rigidbody;
+
+	[SerializeField]
+	private float movementSpeed = 5.0f;
+
+
+	void Awake() {
+		this._transform = this.transform;
+		this._rigidbody = this.rigidbody2D;
+	}
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		float hor = Input.GetAxis("Horizontal");
+		float vert = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2(hor, vert) * this.movementSpeed;
+		this._rigidbody.velocity = movement;
+	}
+}
